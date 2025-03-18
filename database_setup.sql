810d653f8c59843d6bc22e14010e28ed1e5adce0
Michael Scott <michaelcscott2022@gmail.com>
2025-03-18 18:29:28 -0400

    Database setup
    Schema taken from Piazza

--- database_setup.sql
+++ database_setup.sql
@@ -17,3 +17,72 @@ create table if not exists user (
     constraint firstName_min_length check (char_length(trim(firstName)) >= 2),
     constraint lastName_min_length check (char_length(trim(lastName)) >= 2)
 );
+
+CREATE TABLE IF NOT EXISTS post (
+    postId int AUTO_INCREMENT,
+    userId int NOT NULL,
+    postDate date NOT NULL,
+    postText varchar(500) NOT NULL,
+    PRIMARY KEY (postId),
+    FOREIGN KEY (userId) REFERENCES user(userId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT postText_min_length CHECK (char_length(trim(postText)) > 0)
+);
+
+CREATE TABLE IF NOT EXISTS comment (
+    commentId int AUTO_INCREMENT,
+    postId int NOT NULL,
+    userId int NOT NULL,
+    commentDate date NOT NULL,
+    commentText varchar(255) NOT NULL,
+    PRIMARY KEY (commentId),
+    FOREIGN KEY (postId) REFERENCES post(postId)
+        ON DELETE CASCADE,
+    FOREIGN KEY (userId) REFERENCES user(userId),
+    CONSTRAINT commentText_min_length CHECK (char_length(trim(commentText)) > 0)
+);
+
+CREATE TABLE IF NOT EXISTS heart (
+    postId int NOT NULL,
+    userId int NOT NULL,
+    PRIMARY KEY (postId, userId),
+    FOREIGN KEY (postId) REFERENCES post(postId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (userId) REFERENCES user(userId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS bookmark (
+    postId int NOT NULL,
+    userId int NOT NULL,
+    PRIMARY KEY (postId, userId),
+    FOREIGN KEY (postId) REFERENCES post(postId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (userId) REFERENCES user(userId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS hashtag (
+    hashTag varchar(50) NOT NULL,
+    postId int NOT NULL,
+    PRIMARY KEY (hashTag, postId),
+    FOREIGN KEY (postId) REFERENCES post(postId)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS follow (
+    followerUserId int NOT NULL,
+    followeeUserId int NOT NULL,
+    PRIMARY KEY (followerUserId, followeeUserId),
+    FOREIGN KEY (followerUserId) REFERENCES user(userId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (followeeUserId) REFERENCES user(userId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
