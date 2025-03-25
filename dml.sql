9770a6c2b7e4096cb6dcbfa788d09396f97d144b
Nilan Patel <127370252+Nilanap@users.noreply.github.com>
2025-03-24 23:52:00 -0400

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -6,7 +6,7 @@ SELECT p.postId, p.postText, p.postDate, p.userId, "
                 + "JOIN user u ON p.userId = u.userId "
                 + "WHERE f.followerUserId = ? "
                 + "ORDER BY p.postDate DESC
-
+http://localhost:8081
   
 This function handles the /hashtagsearch URL itself.
 SELECT h.postId"
@@ -15,14 +15,20 @@ SELECT h.postId"
                 + " AND h.hashTag IN (<PLACEHOLDER>)"
                 + " GROUP BY h.postId"
                 + " HAVING COUNT(h.hashTag) = ?
+  http://localhost:8081/hashtagsearch?hashtags=%23Demo
+
 
 Liking/Unliking post queries:
 SELECT 1 FROM heart WHERE postId = ? AND userId = ?
 INSERT INTO heart (postId, userId) VALUES (?, ?);
 DELETE FROM heart WHERE postId = ? AND userId = ?;
+http://localhost:8081/post/20
 
 Bookmark / unbookmark:
 SELECT 1 FROM bookmark WHERE userId = ? AND postId = ?
 INSERT INTO bookmark (userId, postId) VALUES (?, ?)
 DELETE FROM bookmark WHERE userId = ? AND postId = ?
 INSERT INTO bookmark (postId, userId) VALUES (?, ?)
+  http://localhost:8081/bookmarks
+
+
